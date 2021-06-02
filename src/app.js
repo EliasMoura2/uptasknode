62c38eb46053e9ae5d30b344ee5c6405cf1c06f3
Elias Moura <d.eliasmoura2@gmail.com>
2021-06-02 10:38:36 -0300

    pug was installed and setted as view engine

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const routes = require('./routes');
 
 const app = express();
+app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
